395f3d9ed2df3884365f3fff3d71a7635fefa2b8
dmytroiliashchuk <diliashchuk@gmail.com>
2023-07-26 18:19:54 +0300

    updates

--- home_task5_2.py
+++ home_task5_2.py
@@ -3,6 +3,16 @@
 # Write a program that takes your name as input, and then your age as input and greets you with the following:
 #“Hello <name>, on your next birthday you’ll be <age+1> years”
 
-name = input("Please write your name ")
-age = input("Please write your age ")
-print(f"Hello {name}, on your next birthday you’ll be {int(age) + 1} years")
+while True:
+    name = input("What is your name? ")
+    if name.isdigit():
+        print("Please write the name, not number")
+    else:
+        break
+while True:
+    age = input("How old are you? ")
+    if not age.isdigit():
+        print("Please write number, not name")
+    else:
+        break
+print("Hello", name.capitalize(), "on your next birthday you’ll be", int(age) + 1, "years ")
