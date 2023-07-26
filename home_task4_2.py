d92b1ef77fe2c5a5fa524ee19c6f2b39141e9720
dmytroiliashchuk <diliashchuk@gmail.com>
2023-07-26 12:13:10 +0300

    mistake

--- home_task4_2.py
+++ home_task4_2.py
@@ -1,8 +1,8 @@
-phone_number = "421222212"
+phone_number = "4212ууаа22212"
 
 if phone_number.isdigit() and len(phone_number) == 10:
     message = "right"
-elif len(phone_number) != 10:
+elif phone_number.isdigit() and len(phone_number) != 10:
         message = "you need write 10 symbol in a phone number"
 else:
     message = "please write correct number without symbols"
