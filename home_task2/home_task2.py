85fbe13d00dc35d524f32595fa380314844fe538
dmytroiliashchuk <diliashchuk@gmail.com>
2023-07-31 17:36:21 +0300

    updates

---
+++ home_task2/home_task2.py
@@ -0,0 +1,23 @@
+#Task 2
+#Create a python program named "task2", and use the built-in function 'print' in it several times.
+#Try to pass "sep", "end" params and pass several parameters separated by commas.
+#Also, provide a comment text above each print statement, mentioned above,
+#with the expected output after execution of the particular print statement.
+
+print("hello world", sep = '  ', end=' 123')
+
+
+
+#Write a program, which has two print statements to print the following text
+#(capital letters "O" and "H", made from "#" symbols):
+
+print("\n\n#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
+# print("#\t\t#")
+# print("#\t\t#")
+# print("#\t\t#")
+# print("#########", end='\n')
+#print("#\t\t#")
+#print("#\t\t#")
+#print("#########")
+#print("#\t\t#")
+#print("#\t\t#")
