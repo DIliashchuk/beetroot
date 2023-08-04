e56426fc8d953e688da9f6d65889c70bd7ed5507
dmytroiliashchuk <diliashchuk@gmail.com>
2023-08-04 18:53:40 +0300

    updates

--- home_task7/hometask7_additional_workers.py
+++ home_task7/hometask7_additional_workers.py
@@ -1,6 +1,12 @@
 import random
 
 java_production = 150
+original_java_production = java_production
+java_production_decreased = False
+
+time_to_work = 60
+music_start_time = random.randint(10, 25)
+music_stop_time = music_start_time + random.randint(10, 25)
 
 python_worker = {
     'min_production': 90,
@@ -8,8 +14,7 @@ python_worker = {
     'time_to_max': 45,
     'current_production': 0,
     'total_production': 0,
-    'loss_of_concentration': random.randint(5, 15),
-    'music_start_time': random.randint(10, 25)
+    'loss_of_concentration': music_start_time,
 }
 
 js_worker = {
@@ -18,11 +23,11 @@ js_worker = {
     'time_to_max': 50,
     'current_production': 0,
     'total_production': 0,
-    'reduction_start_time': python_worker['music_start_time']  # Start reducing with music
+    'reduction_start_time': music_start_time  # Start reducing with music
 }
 
-time_to_work = 60
-title = " time | java_current_production | python_current_production | js_current_production | java_total_production | python_total_production | js_total_production"
+
+title = " time | is_music | java_current_production | python_current_production | js_current_production | java_total_production | python_total_production | js_total_production"
 
 python_change_per_second = (python_worker['max_production'] - python_worker['min_production']) / python_worker['time_to_max']
 python_change_per_second = round(python_change_per_second)
@@ -55,10 +60,18 @@ java_total, python_total, js_total = 0, 0, 0
 print(title)
 for sec in range(time_to_work):
     time = sec
+    is_music = music_start_time <= sec < music_stop_time
+
+    # Simulate music stopping effect for Java worker
+    if is_music:
+        java_production *= 0.9
+    else:
+        java_production = original_java_production  # Restore original Java production value
+
     python_worker['current_production'] = python_timeline[sec]
 
     # Simulate Python worker's faster form with music
-    if python_worker['music_start_time'] <= sec < python_worker['music_start_time'] + 10:
+    if js_worker['reduction_start_time'] <= sec < music_stop_time:
         python_worker['current_production'] *= 2
         python_worker['max_production'] = 150  # Reduced max_production during music
     else:
@@ -68,16 +81,17 @@ for sec in range(time_to_work):
     python_worker['current_production'] = min(python_worker['current_production'], python_worker['max_production'])
 
     # Add random fluctuation to Java worker's productivity
-    java_production += random.randint(-10, 10)
+    java_production = java_production + random.randint(-10, 10)
 
     js_worker['current_production'] = js_timeline[sec]
 
-    # Simulate Java worker's loss of average productivity
-    if sec == python_worker['loss_of_concentration']:
-        java_production *= 0.8
+    # Simulate JS worker's decreasing productivity
+    if sec >= js_worker['reduction_start_time']:
+        current_prod = js_worker['max_production'] - (sec - js_worker['reduction_start_time']) * js_change_per_second
+        js_worker['current_production'] = max(current_prod, js_worker['min_production'])
 
     java_total = java_total + java_production
     python_total = python_total + python_worker['current_production']
     js_total = js_total + js_worker['current_production']
 
-    print(f'{str(time).center(6)}| {str(round(java_production)).center(24)}| {str(round(python_worker["current_production"])).center(26)}| {str(round(js_worker["current_production"])).center(26)}| {str(round(java_total)).center(22)}| {str(round(python_total)).center(24)}| {str(round(js_total)).center(24)}')
+    print(f'{str(time).center(6)}| {str(is_music).center(9)}| {str(round(java_production)).center(24)}| {str(round(python_worker["current_production"])).center(26)}| {str(round(js_worker["current_production"])).center(26)}| {str(round(java_total)).center(22)}| {str(round(python_total)).center(24)}| {str(round(js_total)).center(24)}')
