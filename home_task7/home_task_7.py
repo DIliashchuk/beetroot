48996be842651dc9cab0a5b9914e81b2422b857e
dmytroiliashchuk <diliashchuk@gmail.com>
2023-08-04 15:40:26 +0300

    updates

--- home_task7/home_task_7.py
+++ home_task7/home_task_7.py
@@ -61,6 +61,11 @@ for y, element in enumerate(list_square):
 
 list_weekend = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-for day, number in enumerate(list_weekend):
-    print(day+1, number)
+# Створити словник {1: "Monday", 2: "Tuesday", ...}
+dict_weekday_to_number = {day+1: day_name for day, day_name in enumerate(list_weekend)}
+print(dict_weekday_to_number)
+
+# Створити зворотний словник {"Monday": 1, "Tuesday": 2, ...}
+dict_number_to_weekday = {day_name: day+1 for day, day_name in enumerate(list_weekend)}
+print(dict_number_to_weekday)
 
